0deac74f3468b2e315d9efb758942b0b921c6afd
Brien Thomas <thomasbrien94@gmail.com>
2022-06-13 14:37:24 -0700

    CityNameInput.js passes up the value of the input to app.js, which changes the displayed city name

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ function App() {
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
         <SloganForm setSlogans={setSlogans} slogans={slogans} />
+        <SloganList slogans={slogans} />
       </div>
     </div>
   );
